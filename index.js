d848121eaec4e52e2244767263dc10e4f6b939c2
emanelkafrawy <emanelkafrawy2018@gmail.com>
2020-07-15 00:25:59 +0200

    using mongo DB

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const http = require('http');
+const moragn = require('morgan');
 
 const hostname = 'localhost';
 const port = 2000;
 
 const app = express();
+app.use(moragn('dev'));
+
+app.use(express.static(__dirname+ '/public'));
+
 
 app.use((req, res, next )=> { //next is an optional parameter 
     
-    console.log(req.headers);
+    //console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type','text/html');
     res.end("<html><body><h1>this is an express server</h1></body></html>");
